44d469d5e73f2963c2e628d26a57bd3039dc973a
Vedprakash321 <131158289+Vedprakash321@users.noreply.github.com>
2023-10-03 20:41:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+// Function to update width and height values and display them
+ const sizeInfo = document.getElementById("sizeInfo");
+const widthSpan = document.getElementById("width");
+const heightSpan = document.getElementById("height");
+
+// Function to update window size in the h1 tag
+function updateWindowSize() {
+	widthSpan.textContent = window.innerWidth;
+	heightSpan.textContent = window.innerHeight;
+}
+
+        // Call the function to initialize the size
+        updateWindowSize();
+
+        // Add event listener for window resize
+        window.addEventListener("resize", updateWindowSize);
